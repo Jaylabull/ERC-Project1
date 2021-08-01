cb074fd4808665a7bf7789a0116fab1baddba3f9
Jaylabull <jaylabull@gmail.com>
2021-08-01 13:14:41 -0400

    Connections validated and updated

--- src/main/java/ReimbursementDriver.java
+++ src/main/java/ReimbursementDriver.java
@@ -20,44 +20,49 @@ public class ReimbursementDriver {
 		ReimbursementDaoDB rDao = new ReimbursementDaoDB();
 		
 				
-		UserRole ur1 = new UserRole("EMPLOYEE");
-		UserRole ur2 = new UserRole("FINANCIAL MANAGER");
+		UserRole ur1 = new UserRole(1, "EMPLOYEE");
+		UserRole ur2 = new UserRole(2, "FINANCIAL MANAGER");
 		
-		ReimbursementStatus rs1 = new ReimbursementStatus("PENDING");
-		ReimbursementStatus rs2 = new ReimbursementStatus("APPROVED");
-		ReimbursementStatus rs3 = new ReimbursementStatus("DENIED");
+		ReimbursementStatus rs1 = new ReimbursementStatus(1, "PENDING");
+		ReimbursementStatus rs2 = new ReimbursementStatus(2, "APPROVED");
+		ReimbursementStatus rs3 = new ReimbursementStatus(3, "DENIED");
 		
 		
-		ReimbursementType rt1 = new ReimbursementType("TRAVEL");
-		ReimbursementType rt2 = new ReimbursementType("FOOD");
-		ReimbursementType rt3 = new ReimbursementType("LODGING");
-		ReimbursementType rt4 = new ReimbursementType("OTHER");
+		ReimbursementType rt1 = new ReimbursementType(1, "TRAVEL");
+		ReimbursementType rt2 = new ReimbursementType(2, "FOOD");
+		ReimbursementType rt3 = new ReimbursementType(3, "LODGING");
+		ReimbursementType rt4 = new ReimbursementType(4, "OTHER");
 		
-		//Reimbursement r = new Reimbursement(320, uDao.selectByUserId(0), uDao.selectByUserId(0), "Road trip to Cali", rt1, rs3);
-
 
 		User u = new User("Jazmine", "Sullivan", "js@mail.com", "password", ur1);
 		User u1 = new User("Mike", "Jones", "mj56@mail.com", "password", ur1);
 		User u2 = new User("Warren", "Buffet", "wb@mail.com", "password", ur2);
+		User u3 = new User("Lofti", "Hiphop", "lhb@mail.com", "password", ur1);
+		User u4 = new User("String", "Builder", "sb@mail.com", "password", ur2);
+		User u5 = new User("Mickey", "Mouse", "mm@mail.com", "password", ur2);
 		
 		uDao.createUser(u);
 		uDao.createUser(u1);
 		uDao.createUser(u2);
+		uDao.createUser(u3);
+		uDao.createUser(u4);
+		uDao.createUser(u5);
 		
 		
 		Reimbursement r = new Reimbursement(320, u, u2, "Road trip to Cali", rt1, rs3);
 		rDao.createReim(r);
 		
-		Reimbursement r1 = new Reimbursement(115, "Dinner at STK ATL", rt2, rs1);
+		Reimbursement r1 = new Reimbursement(115, u2, u4 ,"Dinner at STK ATL", rt2, rs1);
 		rDao.createReim(r1);
 		
-		Reimbursement r2 = new Reimbursement(550, "Trip to NYC", rt1, rs2);
+		Reimbursement r2 = new Reimbursement(550, u4, u5, "Trip to NYC", rt1, rs2);
 		rDao.createReim(r2);
 		
-		Reimbursement r3 = new Reimbursement(675, "Hotel at DoubleTree LA", rt3, rs3);
+		Reimbursement r3 = new Reimbursement(675, u3, u4, "Hotel at DoubleTree LA", rt3, rs3);
 		rDao.createReim(r3);
 		
 		List<Reimbursement> rList = new ArrayList<Reimbursement>();
+		rList.add(r);
 		rList.add(r1);
 		rList.add(r2);
 		rList.add(r3);
